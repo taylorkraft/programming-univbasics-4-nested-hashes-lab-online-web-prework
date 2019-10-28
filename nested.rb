6a5059f11fb4555fae1f26dec2bcab64b5a0a1ff
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-28 01:57:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,11 +80,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
      programmer_hash[:yukihiro_matsumoto] = {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }
  return programmer_hash
+ 
 end
   
 
@@ -104,8 +106,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     programmer_hash[:alan_kay][:known_for] = 
+     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
      
 end
