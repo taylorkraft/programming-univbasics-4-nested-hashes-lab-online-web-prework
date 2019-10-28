244badd35559deedfbc55b9d2875a65fdbed7655
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-28 01:52:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash[:yukihiro_matsumoto] = {
+     Hash.new programmer_hash[:yukihiro_matsumoto] = {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }
