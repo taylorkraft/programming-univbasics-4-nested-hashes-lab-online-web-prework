6addc8fc9224e017b7bb04530b0d6a6a211115ee
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-28 02:03:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -120,5 +120,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     return programmer_hash
 end
